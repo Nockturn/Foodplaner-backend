86d49aec8e00a9f969e0f371b4ccf955d9450fe4
Andreas Tudzynski <andreastudzynskiyahoo.de>
2019-10-23 18:28:05 +0200

    Implemented some patterns

--- app.py
+++ app.py
@@ -1,45 +1,23 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
+import controller.productsController as productsControllerModule
 import requests
 import json
 import re
+import os
+import sqlite3
 
 app = Flask(__name__)
 CORS(app)
-url = 'https://www.foodrepo.org/api/v3/products'
+app.config.from_pyfile(os.path.join(".", "config/app.conf"), silent=False)
+
 @app.route('/')
 def index():
-  return 'Server Works!'
+  return app.config.get('PRODUCTS_SECRET')
   
 @app.route('/products', methods=['GET'])
 def getProducts():
-  querystring = request.args
-
-  payload = ""
-  headers = {
-      'Content-Type': 'application/json',
-      'Authorization': 'Token token=aeab5454535b1ee71a073111698dd871'
-  }
-
-  response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
-  data = json.loads(response.text)
-
-  pages = []
-  pager = {}
-  for x in range(int(re.search('=(.*)&page', data['links']['last']).group(1))):
-        pages.append(x)
-  
-  pager['currentpage'] = int(re.search('=(.*)&page', data['links']['self']).group(1))
-  pager['totalItems'] = int(re.search('=(.*)&page', data['links']['last']).group(1)) * 10
-  pager['pageSize'] = 10
-  pager['totalPages'] = int(re.search('=(.*)&page', data['links']['last']).group(1)) / 2
-  pager['startPage'] = 1
-  pager['endPage']= int(re.search('=(.*)&page', data['links']['last']).group(1))
-  pager['pages'] = pages
-  data['pager'] = pager
-  resdata = data
-  pages.pop(0)
-
+  resdata = productsControllerModule.getAllProducts(app.config.get('PRODUCTS_SECRET'))
   res = app.response_class(
     response=json.dumps(resdata),
     status=200,
---
+++ controller/productsController.py
@@ -0,0 +1,31 @@
+import requests
+import json
+import re
+import os
+
+url = 'https://www.foodrepo.org/api/v3/products'
+
+def getAllProducts(secret):
+    payload = ""
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token token=' + secret
+    }
+
+    response = requests.request("GET", url, data=payload, headers=headers)
+    data = json.loads(response.text)
+
+    pages = []
+    pager = {}
+    for x in range(int(re.search('=(.*)&page', data['links']['last']).group(1))):
+            pages.append(x)
+    pages.pop(0)
+    pager['currentpage'] = int(re.search('=(.*)&page', data['links']['self']).group(1))
+    pager['totalItems'] = int(re.search('=(.*)&page', data['links']['last']).group(1)) * 10
+    pager['pageSize'] = 10
+    pager['totalPages'] = int(re.search('=(.*)&page', data['links']['last']).group(1)) / 2
+    pager['startPage'] = 1
+    pager['endPage']= int(re.search('=(.*)&page', data['links']['last']).group(1))
+    pager['pages'] = pages
+    data['pager'] = pager
+    return data
